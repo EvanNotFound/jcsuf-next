9d1a1473d02efd4032f1fd59d80cb97d6815fb4c
Evan Luo <contact@ohevan.com>
2023-07-09 23:28:40 -0400

    utf8 but not working

--- src/lib/updateDescription.tsx
+++ src/lib/updateDescription.tsx
@@ -2,7 +2,7 @@ export default async function updateDescription(description: string) {
 	const res = await fetch("https://api.jcsuf.top/api/updateprofile", {
 		method: "POST",
 		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
+			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
 		},
 		body: `desc=${encodeURIComponent(description)}`,
 		credentials: "include",
--- src/lib/useLoginStatus.tsx
+++ src/lib/useLoginStatus.tsx
@@ -1,16 +1,26 @@
 import useSWR from "swr";
-import {mutate} from "swr";
 
-const fetcher = (url: string) => fetch(url, {method: "GET",credentials: "include",}).then(res => res.json())
+const fetcher = (url: string) =>
+	fetch(url, {
+		method: "GET",
+		credentials: "include",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+		},
+	}).then((res) => res.json());
 
 export default function useLoginStatus() {
-	const { data, error,isLoading, mutate, isValidating } = useSWR('https://api.jcsuf.top/api/loginstatus', fetcher);
+	const { data, error, isLoading, mutate, isValidating } = useSWR(
+		"https://api.jcsuf.top/api/loginstatus",
+		fetcher
+	);
+	console.log(data);
 
 	return {
 		user: data,
 		error,
 		isLoading,
 		mutate,
-		isValidating
-	}
+		isValidating,
+	};
 }
